b686b81e1deefca04471bef3785a676450e83377
Darren Bruning <darren@bruning.net.nz>
2016-02-06 07:46:04 +1300

    Add SmartSolar.Device project, which is just a renamed HelloUWP sample project from Caliburn.Micro. Want to use Caliburn.Micro for the messagebus (EventAggregator) and general MVVM pattern / separation of concerns.

---
+++ SmartSolar.Device/Views/DeviceView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+
+namespace SmartSolar.Device.Views
+{
+    public sealed partial class DeviceView
+    {
+        public DeviceView()
+        {
+            InitializeComponent();
+        }
+
+        private void Test(object sender, RoutedEventArgs e)
+        {
+            Title.Text = "Cliced";
+        }
+    }
+}
---
+++ SmartSolar.Device/Views/ShellView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+
+namespace SmartSolar.Device.Views
+{
+    public sealed partial class ShellView
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenNavigationView(object sender, RoutedEventArgs e)
+        {
+            NavigationView.IsPaneOpen = true;
+        }
+    }
+}
