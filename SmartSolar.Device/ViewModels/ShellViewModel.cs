e7a7f16253aa6f2ccab2eae100c8bc427edfe56c
Darren Bruning <darren@bruning.net.nz>
2016-02-06 08:35:28 +1300

    Add a core and a test project

--- SmartSolar.Device/ViewModels/ShellViewModel.cs
+++ SmartSolar.Device/ViewModels/ShellViewModel.cs
@@ -5,62 +5,62 @@ using SmartSolar.Device.Views;
 
 namespace SmartSolar.Device.ViewModels
 {
-    public class ShellViewModel : Screen, IHandle<ResumeStateMessage>, IHandle<SuspendStateMessage>
-    {
-        private readonly WinRTContainer _container;
-        private readonly IEventAggregator _eventAggregator;
-        private INavigationService _navigationService;
-        private bool _resume;
+	public class ShellViewModel : Screen, IHandle<ResumeStateMessage>, IHandle<SuspendStateMessage>
+	{
+		private readonly WinRTContainer _container;
+		private readonly IEventAggregator _eventAggregator;
+		private INavigationService _navigationService;
+		private bool _resume;
 
 		public MainPageView MainPage { get; set; }
 
-        public ShellViewModel(WinRTContainer container, IEventAggregator eventAggregator)
-        {
-            _container = container;
-            _eventAggregator = eventAggregator;
+		public ShellViewModel(WinRTContainer container, IEventAggregator eventAggregator)
+		{
+			_container = container;
+			_eventAggregator = eventAggregator;
 			MainPage = new MainPageView();
-        }
+		}
 
-        protected override void OnActivate()
-        {
-            _eventAggregator.Subscribe(this);
-        }
+		protected override void OnActivate()
+		{
+			_eventAggregator.Subscribe(this);
+		}
 
-        protected override void OnDeactivate(bool close)
-        {
-            _eventAggregator.Unsubscribe(this);
-        }
+		protected override void OnDeactivate(bool close)
+		{
+			_eventAggregator.Unsubscribe(this);
+		}
 
-        public void SetupNavigationService(Frame frame)
-        {
-            _navigationService = _container.RegisterNavigationService(frame);
+		public void SetupNavigationService(Frame frame)
+		{
+			_navigationService = _container.RegisterNavigationService(frame);
 
-	        if (_resume)
-	        {
-		        _navigationService.ResumeState();
-	        }
-	        else
-	        {
+			if (_resume)
+			{
+				_navigationService.ResumeState();
+			}
+			else
+			{
 				_navigationService.For<MainPageViewModel>().Navigate();
-		        
-	        }
+				
+			}
 					;
-        }
+		}
 
-        public void ShowDevices()
-        {
-//            _navigationService.For<DeviceViewModel>().Navigate();
-            _navigationService.For<MainPageViewModel>().Navigate();
-        }
+		public void ShowDevices()
+		{
+			_navigationService.For<DeviceViewModel>().Navigate();
+//            _navigationService.For<MainPageViewModel>().Navigate();
+		}
 
-        public void Handle(SuspendStateMessage message)
-        {
-            _navigationService.SuspendState();
-        }
+		public void Handle(SuspendStateMessage message)
+		{
+			_navigationService.SuspendState();
+		}
 
-        public void Handle(ResumeStateMessage message)
-        {
-            _resume = true;
-        }
-    }
+		public void Handle(ResumeStateMessage message)
+		{
+			_resume = true;
+		}
+	}
 }
