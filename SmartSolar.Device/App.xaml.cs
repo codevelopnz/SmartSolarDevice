0bf7f8e7e6345a023a907d4f3d6143e6e8c17297
Darren Bruning <darren@bruning.net.nz>
2016-02-21 06:57:02 +1300

    Remove old container & tidy

--- SmartSolar.Device/App.xaml.cs
+++ SmartSolar.Device/App.xaml.cs
@@ -14,7 +14,6 @@ namespace SmartSolar.Device
 {
 	public sealed partial class App
 	{
-		private WinRTContainer _container;
 		private IEventAggregator _eventAggregator;
 		private IKernel _kernel;
 
@@ -30,34 +29,14 @@ namespace SmartSolar.Device
 			_kernel.Bind<IEventAggregator>().To<EventAggregator>().InSingletonScope();
 			_eventAggregator = _kernel.Get<IEventAggregator>();
 
-			_container = new WinRTContainer();
-			_container.RegisterWinRTServices();
+//			_container = new WinRTContainer();
+//			_container.RegisterWinRTServices();
 
-//			_eventAggregator = _container.GetInstance<IEventAggregator>();
 			// Singletons
-//			_container
-//				.Singleton<Settings>()
-//				.Singleton<PumpController>();
 			_kernel.Bind<Settings>().ToSelf().InSingletonScope();
 			_kernel.Bind<PumpController>().ToSelf().InSingletonScope();
 			var settings = _kernel.Get<Settings>();
 
-			// Per-requests
-//			_container
-//				.PerRequest<ShellViewModel>()
-//				.PerRequest<DeviceViewModel>()
-//				.PerRequest<MainPageViewModel>()
-//				.PerRequest<ReadoutViewModel>();
-
-
-			// Workaround for Caliburn Micro being dumb about CI, TODO link to explanation about how it doesn't like to instantiate classes where the ancestor has a public controller. Switch to ninject after all?
-//			PumpController singletonPumpController;
-//			_container.Handler<PumpController>(c => {
-//				if (singletonPumpController != null) return singletonPumpController; 
-//				)
-//			})
-//				>()
-//				.
 			// Use real hardware if we have a GpioController - else use fakeys
 			var shouldUseRealHardware = (GpioController.GetDefault() != null);
 
@@ -68,47 +47,16 @@ namespace SmartSolar.Device
 				var pumpGpioPin = gpioController.OpenPin(settings.PumpGpioPin);
 
 				// Create an output connection on top of the pins
-				_container.PerRequest<GpioOutputConnection>();
-				var pumpConnection = _container.GetInstance<GpioPumpOutputConnection>();
+				var pumpConnection = _kernel.Get<GpioPumpOutputConnection>();
 				pumpConnection.Configure(pumpGpioPin);
 
 				// Use these connections where requested
-//				pump.Connection = pumpConnection;
 				_kernel.Bind<IPumpOutputConnection>().ToConstant(pumpConnection);
 			} else {
 				// Register some fake connectors
-//				_container.PerRequest<FakeOutputConnection>();
-
-//				var fakePumpConnection = _kernel.Get<FakeOutputConnection>();
-//				_container.RegisterInstance(typeof (IPumpOutputConnection), null, fakePumpConnection);
 				_kernel.Bind<IPumpOutputConnection>().To<FakePumpOutputConnection>();
 			}
 
-			// Intercept the creation of any object, and configure it before it gets used.
-			// https://caliburnmicro.codeplex.com/wikipage?title=The%20Simple%20IoC%20Container
-//			_container.Activated += o =>
-//			{
-//				// Configure the PumpController with either a real or fake output connection
-//				var pump = o as PumpController;
-//				if (pump != null)
-//				{
-//					if (shouldUseRealHardware)
-//					{
-//						// Get a real GPIO pin
-//						var gpioController = GpioController.GetDefault();
-//						var pumpGpioPin = gpioController.OpenPin(settings.PumpGpioPin);
-//						// Create an output connection on top of the pin
-//						var pumpConnection = _container.GetInstance<GpioOutputConnection>();
-//						pumpConnection.Configure(pumpGpioPin);
-//						pump.Connection = pumpConnection;
-//					}
-//					else
-//					{
-//						// Use a fake output connection
-//						pump.Connection = _container.GetInstance<FakeOutputConnection>();
-//					}
-//				}
-//			};
 		}
 
 
