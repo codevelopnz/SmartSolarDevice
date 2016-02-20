126fccfe4152d8e49e517bd386a4fa9f0cb01c2a
Darren Bruning <darren@bruning.net.nz>
2016-02-21 07:51:11 +1300

    Add element to readout

--- SmartSolar.Device/ViewModels/ReadoutViewModel.cs
+++ SmartSolar.Device/ViewModels/ReadoutViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using SmartSolar.Device.Core.Common;
 using SmartSolar.Device.Core.Pump;
 
 namespace SmartSolar.Device.ViewModels
@@ -11,19 +12,30 @@ namespace SmartSolar.Device.ViewModels
 	public class ReadoutViewModel : Screen
 	{
 		public PumpController PumpController { get; set; }
+		public Hardware Hardware { get; set; }
 
 		public string PumpStateText => 
 			PumpController.IsPumping.HasValue 
 			? (PumpController.IsPumping.Value ? "On" : "Off") 
 			: "?";
+		public string ElementStateText => 
+			Hardware.ElementOutputConnection.State.HasValue
+			? (Hardware.ElementOutputConnection.State.Value ? "On" : "Off") 
+			: "?";
 
-		public ReadoutViewModel(PumpController pumpController)
+		public ReadoutViewModel(
+			PumpController pumpController,
+			Hardware hardware
+			)
 		{
 			PumpController = pumpController;
+			Hardware = hardware;
 
 			// When the PumpController changes state, so should our text, so notify of that change.
 			PumpController.PropertyChanged +=
 				(s, e) => { if (e.PropertyName == nameof(PumpController.IsPumping)) NotifyOfPropertyChange(() => PumpStateText); };
+			Hardware.ElementOutputConnection.PropertyChanged += (s, e) => NotifyOfPropertyChange(() => nameof(ElementStateText));
+
 		}
 	}
 }
