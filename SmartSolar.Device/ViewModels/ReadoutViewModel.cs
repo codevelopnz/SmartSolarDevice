9f207101f46a46ed8785bc3b49a59fe1eddde1b9
Darren Bruning <darren@bruning.net.nz>
2016-02-23 20:57:02 +1300

    Add other temps to readout view

--- SmartSolar.Device/ViewModels/ReadoutViewModel.cs
+++ SmartSolar.Device/ViewModels/ReadoutViewModel.cs
@@ -27,6 +27,12 @@ namespace SmartSolar.Device.ViewModels
 		public string RoofDegC => Hardware.RoofTemperatureReader.LastTemperatureDegC.HasValue
 			? Hardware.RoofTemperatureReader.LastTemperatureDegC.ToString()
 			: "?";
+		public string TankDegC => Hardware.TankTemperatureReader.LastTemperatureDegC.HasValue
+			? Hardware.TankTemperatureReader.LastTemperatureDegC.ToString()
+			: "?";
+		public string InletDegC => Hardware.InletTemperatureReader.LastTemperatureDegC.HasValue
+			? Hardware.InletTemperatureReader.LastTemperatureDegC.ToString()
+			: "?";
 
 
 
@@ -39,10 +45,11 @@ namespace SmartSolar.Device.ViewModels
 			Hardware = hardware;
 
 			// When the PumpController changes state, so should our text, so notify of that change.
-			PumpController.PropertyChanged +=
-				(s, e) => { if (e.PropertyName == nameof(PumpController.IsPumping)) NotifyOfPropertyChange(() => PumpStateText); };
+			PumpController.PropertyChanged += (s, e) => { if (e.PropertyName == nameof(PumpController.IsPumping)) NotifyOfPropertyChange(() => PumpStateText); };
 			Hardware.ElementOutputConnection.PropertyChanged += (s, e) => NotifyOfPropertyChange(() => ElementStateText);
 			Hardware.RoofTemperatureReader.PropertyChanged += (s, e) => NotifyOfPropertyChange(() => RoofDegC);
+			Hardware.InletTemperatureReader.PropertyChanged += (s, e) => NotifyOfPropertyChange(() => InletDegC);
+			Hardware.TankTemperatureReader.PropertyChanged += (s, e) => NotifyOfPropertyChange(() => TankDegC);
 		}
 
 		public void RoofDegCPlus()
@@ -51,6 +58,46 @@ namespace SmartSolar.Device.ViewModels
 			if (fake != null)
 			{
 				fake.FakeTemperatureDegC += 3;
+			}
+		}
+		public void RoofDegCMinus()
+		{
+			var fake = (Hardware.RoofTemperatureReader as FakeTemperatureReader);
+			if (fake != null)
+			{
+				fake.FakeTemperatureDegC -= 3;
+			}
+		}
+		public void InletDegCPlus()
+		{
+			var fake = (Hardware.InletTemperatureReader as FakeTemperatureReader);
+			if (fake != null)
+			{
+				fake.FakeTemperatureDegC += 3;
+			}
+		}
+		public void InletDegCMinus()
+		{
+			var fake = (Hardware.InletTemperatureReader as FakeTemperatureReader);
+			if (fake != null)
+			{
+				fake.FakeTemperatureDegC -= 3;
+			}
+		}
+		public void TankDegCPlus()
+		{
+			var fake = (Hardware.TankTemperatureReader as FakeTemperatureReader);
+			if (fake != null)
+			{
+				fake.FakeTemperatureDegC += 3;
+			}
+		}
+		public void TankDegCMinus()
+		{
+			var fake = (Hardware.TankTemperatureReader as FakeTemperatureReader);
+			if (fake != null)
+			{
+				fake.FakeTemperatureDegC -= 3;
 			}
 		}
 	}
